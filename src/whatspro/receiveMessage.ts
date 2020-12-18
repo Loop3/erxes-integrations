9685b517cfcbba2d54736772cecf01b6fecab077
Jonatan Rinckus <jonatan@loop3.com.br>
2020-12-17 21:29:07 -0300

    implementing on my external message

--- src/whatspro/receiveMessage.ts
+++ src/whatspro/receiveMessage.ts
@@ -1,3 +1,4 @@
+import * as Random from 'meteor-random';
 import { sendRPCMessage } from '../messageBroker';
 import Integrations from '../models/Integrations';
 import { ConversationMessages, Conversations } from './models';
@@ -19,7 +20,7 @@ const receiveMessage = async (message: any, integrationId: string) => {
       mid: message._id,
     });
 
-    if (conversationMessage)
+    if (conversationMessage) {
       try {
         await sendRPCMessage({
           action: 'update-conversation-message',
@@ -32,6 +33,106 @@ const receiveMessage = async (message: any, integrationId: string) => {
       } catch (e) {
         throw new Error(e);
       }
+    } else {
+      let phoneNumber = message.contact.phone;
+      let name = message.contact.name;
+      let avatarUrl = message.contact.avatarUrl;
+      let content = convertWAToHtml(message.message);
+
+      const customer = await getOrCreateCustomer(phoneNumber, name, instanceId, avatarUrl);
+
+      let conversation = await Conversations.findOne({
+        senderId: customer.id,
+        instanceId,
+      });
+
+      if (!conversation) {
+        try {
+          conversation = await Conversations.create({
+            timestamp: new Date(),
+            senderId: customer.id,
+            recipientId: phoneNumber,
+            content,
+            integrationId: integration._id,
+            instanceId,
+          });
+        } catch (e) {
+          throw new Error(e.message.includes('duplicate') ? 'Concurrent request: conversation duplication' : e);
+        }
+      }
+
+      // save on api
+      try {
+        const apiConversationResponse = await sendRPCMessage({
+          action: 'create-or-update-conversation',
+          payload: JSON.stringify({
+            customerId: customer.erxesApiId,
+            integrationId: integration.erxesApiId,
+            conversationId: conversation.erxesApiId,
+            content,
+          }),
+        });
+
+        conversation.erxesApiId = apiConversationResponse._id;
+
+        await conversation.save();
+      } catch (e) {
+        await Conversations.deleteOne({ _id: conversation._id });
+        throw new Error(e);
+      }
+
+      const id = Random.id();
+
+      // save on integrations db
+      try {
+        await ConversationMessages.create({
+          erxesApiId: id,
+          conversationId: conversation._id,
+          mid: message._id,
+          timestamp: new Date(),
+          content,
+        });
+      } catch (e) {
+        if (e.message.includes('duplicate')) {
+          return receiveMessage(message, integrationId);
+        } else {
+          throw new Error(e);
+        }
+      }
+
+      // save message on api //Todo
+      let attachments = [];
+      if (!['chat', 'vcard'].includes(message.type)) {
+        const attachment = { type: message.type, url: message.fileUrl };
+        attachments = [attachment];
+      }
+
+      try {
+        await sendRPCMessage({
+          action: 'create-conversation-message',
+          metaInfo: 'replaceContent',
+          payload: JSON.stringify({
+            _id: id,
+            content,
+            contentType: message.type === 'vcard' ? 'vcard' : undefined,
+            attachments: (attachments || []).map(att => ({
+              type: att.type,
+              url: att.url,
+            })),
+            conversationId: conversation.erxesApiId,
+            // customerId: customer.erxesApiId,
+            isGroupMsg: message.isGroupMsg,
+            isNewMsg: message.isNewMsg,
+            isMe: true,
+            createdAt: new Date(message.time),
+            status: message.status,
+          }),
+        });
+      } catch (e) {
+        await ConversationMessages.deleteOne({ mid: message.mid });
+        throw new Error(e);
+      }
+    }
   } else if (message.self === 'in') {
     let phoneNumber = message.contact.phone;
     let name = message.contact.name;
